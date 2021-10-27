0f9a4e6ee72c0e19aef46a10ec755bd4c8bdcd39
Mat Cotton Jr <matcottonjr@gmail.com>
2021-10-27 01:34:35 -0500

    Add in shake to css and js, also ? edge case

--- First draft/app.js
+++ First draft/app.js
@@ -1,6 +1,6 @@
 const eightBall = ["magic8ball_1.png", "magic8ball_2.png", "magic8ball_3.png", "magic8ball_4.png", "magic8ball_5.png", "magic8ball_6.png", "magic8ball_7.png", "magic8ball_8.png", "magic8ball_9.png", "magic8ball_10.png", "magic8ball_11.png", "magic8ball_12.png", "magic8ball_13.png", "magic8ball_14.png", "magic8ball_15.png", "magic8ball_16.png", "magic8ball_17.png", "magic8ball_18.png", "magic8ball_19.png", "magic8ball_20.png"];
 
-const min = 0;
+// const min = 0;
 const max = eightBall.length;
 
 const button = document.getElementById('askButton');
@@ -11,15 +11,18 @@ button.addEventListener('click', function ask() {
     const randomIndex = Math.floor(Math.random() * max);
     const randomBall = eightBall[randomIndex];
 
-    if (input.value === "") { alert("WHAT IS YOUR QUESTION?") } else {
+    if (input.value === "" || !input.value.endsWith("?")) { alert("WHAT IS YOUR QUESTION?") } else {
         const newInput = input.value;
         input.placeholder = newInput;
+        img.classList.add("apply-shake");
         setTimeout(() => {
             img.src = randomBall;
+            input.value = "";
         }, 2000);
     }
 });
 
 input.addEventListener('click', () => {
     input.value = "";
+    img.classList.remove("apply-shake");
 });
