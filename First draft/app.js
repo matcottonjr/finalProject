e35b0538f7596b3bfe784167e30c6d7eecf585e1
Mat Cotton Jr <matcottonjr@gmail.com>
2021-10-28 11:31:36 -0500

    Delete img 3, adjust js accordingly, align button color

--- First draft/app.js
+++ First draft/app.js
@@ -1,4 +1,4 @@
-const eightBall = ["magic8ball_1.png", "magic8ball_2.png", "magic8ball_3.png", "magic8ball_4.png", "magic8ball_5.png", "magic8ball_6.png", "magic8ball_7.png", "magic8ball_8.png", "magic8ball_9.png", "magic8ball_10.png", "magic8ball_11.png", "magic8ball_12.png", "magic8ball_13.png", "magic8ball_14.png", "magic8ball_15.png", "magic8ball_16.png", "magic8ball_17.png", "magic8ball_18.png", "magic8ball_19.png", "magic8ball_20.png"];
+const eightBall = ["magic8ball_1.png", "magic8ball_2.png", "magic8ball_4.png", "magic8ball_5.png", "magic8ball_6.png", "magic8ball_7.png", "magic8ball_8.png", "magic8ball_9.png", "magic8ball_10.png", "magic8ball_11.png", "magic8ball_12.png", "magic8ball_13.png", "magic8ball_14.png", "magic8ball_15.png", "magic8ball_16.png", "magic8ball_17.png", "magic8ball_18.png", "magic8ball_19.png", "magic8ball_20.png"];
 
 // const min = 0;
 const max = eightBall.length;
